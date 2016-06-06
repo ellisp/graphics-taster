af77336ed34cb56ea7f86399564b908b866b0067
ellisp <peter.ellis2013nz@gmail.com>
2016-06-06 16:48:10 +1200

    getting started on presentation

--- build.R
+++ build.R
@@ -2,16 +2,5 @@ library(knitr)
 library(rmarkdown)
 library(shinyapps)
 
-# Note.  The data in /data/ is all created in the separate Graphics Fundamentals
-# repository and has just been copied in.
-
-deployApp(paste0(projdir, "/shiny1"), appName = "DensityDemo", account = "ellisp")
-
-setwd(paste0(projdir, "/Session1"))
-render("session1.Rmd")
-setwd(paste0(projdir, "/Session2"))
-render("session2.Rmd")
-
-setwd(paste0(projdir, "/Session5"))
-render("session5.Rmd")
+render("graphics-ird.Rmd")
 
